2c752e4390bb126f84596476291b102ee09cd393
Rebbeccka <kozmovarebecca11@gmail.com>
2023-08-01 21:51:19 +0200

    refractor proper naming

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from avl_map import AVLMap
 from zobraz_menu import display_menu
 
 def main():
-    avl_mapa = AVLMap()
-
+    avl_map = AVLMap()
 
     while True:
         display_menu()
@@ -13,32 +12,32 @@ def main():
         if choice == "1":
             key = input("Zadajte kľúč: ")
             value = input("Zadajte hodnotu: ")
-            avl_mapa.insert(key, value)
+            avl_map.insert(key, value)
             print("Kľúč a value boli vložené do mapy.")
         elif choice == "2":
             key = input("Zadajte kľúč, pre ktorý chcete získať hodnotu: ")
-            if key in avl_mapa:
-                value = avl_mapa.get(key)
+            if key in avl_map:
+                value = avl_map.get(key)
                 print(f"Hodnota pre kľúč '{key}': {value}")
             else:
                 print(f"Kľúč '{key}' nebol nájdený v mape.")
         elif choice == "3":
             key = input("Zadajte kľúč, ktorý chcete skontrolovať: ")
-            if key in avl_mapa:
+            if key in avl_map:
                 print(f"Kľúč '{key}' je obsiahnutý v mape.")
             else:
                 print(f"Kľúč '{key}' nie je obsiahnutý v mape.")
         elif choice == "4":
             key = input("Zadajte kľúč, ktorý chcete odstrániť: ")
             try:
-                del avl_mapa[key]
+                del avl_map[key]
                 print(f"Kľúč '{key}' bol úspešne odstránený z mapy.")
             except KeyError:
                 print(f"Kľúč '{key}' nebol nájdený v mape.")
         elif choice == "5":
             print("Obsah mapy:")
-            for key in avl_mapa:
-                print(f"{key}: {avl_mapa[key]}")
+            for key in avl_map:
+                print(f"{key}: {avl_map[key]}")
         elif choice == "6":
             print("Ukončujem program.")
             break
